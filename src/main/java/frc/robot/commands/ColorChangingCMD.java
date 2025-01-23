06fd2fb7af28ef9bdc1a068a560401af923dbbc2
TJ <Timmythom08@gmail.com>
2025-01-23 16:38:33 -0600

    Created auto, must leave

--- src/main/java/frc/robot/commands/ColorChangingCMD.java
+++ src/main/java/frc/robot/commands/ColorChangingCMD.java
@@ -1,41 +1,41 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ColorChanger;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.subsystems.ColorChanger;
 
 
-public class ColorChangingCMD extends Command {
-  private ColorChanger colorChanger;
+// public class ColorChangingCMD extends Command {
+//   private ColorChanger colorChanger;
 
-  /** Creates a new ColorChangingCMD. */
-  public ColorChangingCMD(ColorChanger colorChanger) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.colorChanger = colorChanger;
-    addRequirements(colorChanger);
-  }
+//   /** Creates a new ColorChangingCMD. */
+//   public ColorChangingCMD(ColorChanger colorChanger) {
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     this.colorChanger = colorChanger;
+//     addRequirements(colorChanger);
+//   }
   
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    colorChanger.setColor();
-  }
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     colorChanger.setColor();
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
 
