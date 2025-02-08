ed560b53058af4d770d975baf863224c0ce179ab
XanderGladen <glade003@apps.district279.org>
2025-02-08 08:59:55 -0600

    Added profiledPIDController

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -14,6 +14,8 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
@@ -27,15 +29,16 @@ import frc.robot.Constants;
 
 
 public class ElevatorSubsystem extends SubsystemBase {
-  /** Creates a new IntakeSubsystem. */
+  /** Creates a new ElevatorSubsystem. */
 
   public DutyCycleEncoder elevatorEncoder;
   public DigitalInput elevatorLimitSwitch;
   public PIDController pidController;
   public SparkMax elevatorMotorLeader;
   public SparkMax elevatorMotorFollower;
-  public TrapezoidProfile trapezoidProfile;
+  public TrapezoidProfile.Constraints trapezoidConstraints;
   public ElevatorFeedforward elevatorFeedforward;
+  public ProfiledPIDController profiledPIDController;
   public TrapezoidProfile.State goal;
   public TrapezoidProfile.State setpoint;
 
@@ -70,11 +73,10 @@ public class ElevatorSubsystem extends SubsystemBase {
     this.elevatorLimitSwitch = new DigitalInput(Constants.Elevator.topLimitSwitchID);
 
 
-    this.pidController = new PIDController(Constants.Elevator.PID.kp, Constants.Elevator.PID.ki, Constants.Elevator.PID.kd);
-    // this.pidController.setD(Constants.Elevator.intakePID.kd);
+    // this.pidController.setD(Constants.Elevator.elevatorPID.kd);
     // SendableRegistry.addChild("D", d);
-    // this.pidController.setI(Constants.Elevator.intakePID.ki);
-    // this.pidController.setP(Constants.Elevator.intakePID.kp);
+    // this.pidController.setI(Constants.Elevator.elevatorPID.ki);
+    // this.pidController.setP(Constants.Elevator.elevatorPID.kp);
 
 
     this.pidController.enableContinuousInput(0, 360);
@@ -88,24 +90,19 @@ public class ElevatorSubsystem extends SubsystemBase {
     
     this.elevatorMotorFollower.configure(followerConfig, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
 
-    this.trapezoidProfile = new TrapezoidProfile(
-      new TrapezoidProfile.Constraints(Constants.Elevator.maxVelocity, Constants.Elevator.maxAcceleration)
-      );
+    this.trapezoidConstraints = 
+      new TrapezoidProfile.Constraints(Constants.Elevator.maxVelocity, Constants.Elevator.maxAcceleration);
 
     this.goal = new TrapezoidProfile.State();
     this.setpoint = new TrapezoidProfile.State();
 
     this.elevatorFeedforward = new ElevatorFeedforward(Constants.Elevator.ks, Constants.Elevator.kg, Constants.Elevator.kv, Constants.Elevator.ka);
-    this.trapezoidProfile.calculate(elevatorkd, setpoint, goal);//move this immediately
-    leaderConfig = new SparkMaxConfig();
-    leaderConfig.();
-    goal = new TrapezoidProfile.State(5, 0);
-    pidController.setSetpoint(
-        elevatorEncoder.get(),
-        setpoint.position,
-        elevatorFeedforward.calculate(setpoint.velocity) / 12.0);
+
+    this.profiledPIDController = new ProfiledPIDController(Constants.Elevator.PID.kp, Constants.Elevator.PID.ki, Constants.Elevator.PID.kd, this.trapezoidConstraints);
+    this.profiledPIDController.setGoal(0);
   }
-   
+
+
   public void loadPreferences() {
     if (Preferences.getDouble("elevatorkp", elevatorkp) != elevatorkp) {
       elevatorkp = Preferences.getDouble("elevatorkp", elevatorkp);
@@ -121,41 +118,41 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
   }
 
-  public double giveVoltage(double floor_angle, double current_angle) {
+  public double giveVoltage(TrapezoidProfile.State desired_height, double current_height) {
     // Floor control
-    SmartDashboard.putNumber("originalAngle", current_angle);
+    SmartDashboard.putNumber("originalheight", current_height);
    
-    //double angle = Math.abs(360 - current_angle); //reverses it
-    double angle = current_angle;
-    SmartDashboard.putNumber("updatedAngle", angle);
+    //double height = Math.abs(360 - current_height); //reverses it
+    double height = current_height;
+    SmartDashboard.putNumber("updatedheight", height);
 
-    double intake_floor_voltage = pidController.calculate(angle, floor_angle);
+    double elevator_floor_voltage = profiledPIDController.calculate(height, desired_height) + elevatorFeedforward.calculate(profiledPIDController.getSetpoint().velocity);
 
     // If the Floor is at exactly 0.0, it's probably not connected, so disable it
-    SmartDashboard.putNumber("pid output", intake_floor_voltage);
-    System.out.println("error: " + intake_floor_voltage);
+    SmartDashboard.putNumber("pid output", elevator_floor_voltage);
+    System.out.println("error: " + elevator_floor_voltage);
 
-    //double adjustedIntakefloorVoltage = 10 - Math.abs(intake_floor_voltage);
-    double adjustedIntakeFloorVoltage = intake_floor_voltage; //error reversed for voltage
+    //double adjustedElevatorfloorVoltage = 10 - Math.abs(elevator_floor_voltage);
+    double adjustedElevatorFloorVoltage = elevator_floor_voltage; //error reversed for voltage
     if (elevatorEncoder.get() == 0.0) {
-      adjustedIntakeFloorVoltage = 0.0;
+      adjustedElevatorFloorVoltage = 0.0;
     }
-    if (adjustedIntakeFloorVoltage > Constants.Elevator.maxVoltage) {
-      adjustedIntakeFloorVoltage = Constants.Elevator.maxVoltage;
+    if (adjustedElevatorFloorVoltage > Constants.Elevator.maxVoltage) {
+      adjustedElevatorFloorVoltage = Constants.Elevator.maxVoltage;
     }
-    if (adjustedIntakeFloorVoltage < -Constants.Elevator.maxVoltage) {
-      adjustedIntakeFloorVoltage = -Constants.Elevator.maxVoltage;
+    if (adjustedElevatorFloorVoltage < -Constants.Elevator.maxVoltage) {
+      adjustedElevatorFloorVoltage = -Constants.Elevator.maxVoltage;
     }
 
-    System.out.println("final voltage: " + adjustedIntakeFloorVoltage);
-    return adjustedIntakeFloorVoltage;
+    System.out.println("final voltage: " + adjustedElevatorFloorVoltage);
+    return adjustedElevatorFloorVoltage;
   }
  
-  public void stopIntake() {
+  public void stopElevator() {
     elevatorMotorLeader.set(0);
   }
 
-  public double floorTargetToAngle(FloorTarget target) {
+  public double floorTargetToheight(FloorTarget target) {
     switch (target) {
       case GROUND_FLOOR:
         return Constants.Elevator.groundFloor;
@@ -185,14 +182,16 @@ public class ElevatorSubsystem extends SubsystemBase {
 
 
   public void goToGround() {
-    //  if (getIntakeHasNote()) {
+    //  if (getElevatorHasNote()) {
     //   goToStow();
     // }
     floor_target = FloorTarget.GROUND_FLOOR;
-    double floor_angle = Constants.Elevator.groundFloor;
-    System.out.println("stow angle target: " + floor_angle);
-    System.out.println("final voltage: " + giveVoltage(floor_angle, elevatorEncoder.get()) + "\n\n");
-    double voltage = giveVoltage(floor_angle, elevatorEncoder.get());
+    double desired_height = Constants.Elevator.groundFloor;
+    goal = new TrapezoidProfile.State(desired_height, 0);
+
+    System.out.println("stow height target: " + desired_height);
+    System.out.println("final voltage: " + giveVoltage(goal, elevatorEncoder.get()) + "\n\n");
+    double voltage = giveVoltage(goal, elevatorEncoder.get());
     if (elevatorEncoder.get() < 100 && voltage == -Constants.Elevator.maxVoltage) {
       elevatorMotorLeader.setVoltage(Constants.Elevator.maxVoltage);
     }
