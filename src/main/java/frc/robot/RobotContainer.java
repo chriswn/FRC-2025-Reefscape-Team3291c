3e89f49de92c876aa987716b1de533858d97ec68
TJ <Timmythom08@gmail.com>
2025-02-11 16:21:31 -0600

    Made SImple Auto With PathPlanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -195,7 +195,7 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("Simple Auto");
   }
 
   public void setMotorBrake(boolean brake) {
